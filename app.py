eda156bcdccc322afa0866db4575d8d870a4319d
Vanjavenu <venuvanja1@gmail.com>
2026-09-10 09:57:55 -0700

    Update Student Management System

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+
 
 def create_database():
     connection = sqlite3.connect("students.db")
@@ -28,8 +30,14 @@ def home():
 
     connection = sqlite3.connect("students.db")
     cursor = connection.cursor()
-    cursor.execute("SELECT id, name, age, email, phone, course FROM students")
+
+    cursor.execute("""
+        SELECT id, name, age, email, phone, course
+        FROM students
+    """)
+
     students = cursor.fetchall()
+
     connection.close()
 
     return render_template("index.html", students=students)
@@ -37,6 +45,7 @@ def home():
 
 @app.route("/add", methods=["POST"])
 def add_student():
+
     name = request.form["name"]
     age = request.form["age"]
     email = request.form["email"]
@@ -47,7 +56,8 @@ def add_student():
     cursor = connection.cursor()
 
     cursor.execute("""
-        INSERT INTO students (name, age, email, phone, course)
+        INSERT INTO students
+        (name, age, email, phone, course)
         VALUES (?, ?, ?, ?, ?)
     """, (name, age, email, phone, course))
 
@@ -55,6 +65,65 @@ def add_student():
     connection.close()
 
     return redirect(url_for("home"))
+
+
+@app.route("/delete/<int:student_id>", methods=["POST"])
+def delete_student(student_id):
+
+    connection = sqlite3.connect("students.db")
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "DELETE FROM students WHERE id = ?",
+        (student_id,)
+    )
+
+    connection.commit()
+    connection.close()
+
+    return redirect(url_for("home"))
+
+
+@app.route("/edit/<int:student_id>", methods=["GET", "POST"])
+def edit_student(student_id):
+
+    connection = sqlite3.connect("students.db")
+    cursor = connection.cursor()
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        age = request.form["age"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        course = request.form["course"]
+
+        cursor.execute("""
+            UPDATE students
+            SET name = ?,
+                age = ?,
+                email = ?,
+                phone = ?,
+                course = ?
+            WHERE id = ?
+        """, (name, age, email, phone, course, student_id))
+
+        connection.commit()
+        connection.close()
+
+        return redirect(url_for("home"))
+
+    cursor.execute("""
+        SELECT id, name, age, email, phone, course
+        FROM students
+        WHERE id = ?
+    """, (student_id,))
+
+    student = cursor.fetchone()
+
+    connection.close()
+
+    return render_template("edit.html", student=student)
 
 
 if __name__ == "__main__":
