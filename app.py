0d18cc9ad009dba2f571c10d3252c78d6b9283d9
Vanjavenu <venuvanja1@gmail.com>
2026-09-09 23:17:57 -0700

    Prepare Flask app for deployment

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return render_template("index.html")
-
+    connection = sqlite3.connect("students.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT id, name, age, email, phone, course FROM students")
+    students = cursor.fetchall()
+    connection.close()
+    return render_template("index.html", students=students)
 
 if __name__ == "__main__":
     app.run(debug=True)
